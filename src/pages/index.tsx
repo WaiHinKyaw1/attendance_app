58869e43ea5b6be797f3bac0a1e0e815c7b19405
WaiHinKyaw1 <waihinkyaw2580@gmail.com>
2025-06-08 20:19:55 +0630

    update index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-blue-500 p-8">
       <h1 className="text-2xl text-blue-800 font-bold mb-6" >Attendance Dashboard</h1>
+      <h2>Hello</h2>
     </main>
   );
 }
