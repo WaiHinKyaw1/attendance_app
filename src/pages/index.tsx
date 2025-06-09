131112041b1a29b806dd464d4301a9b85a730435
WaiHinKyaw1 <waihinkyaw2580@gmail.com>
2025-06-09 16:02:40 +0630

    update code icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 
+import { HiBuildingLibrary, HiCog6Tooth, HiMiniClock, HiUser } from "react-icons/hi2";
+
 
 export default function Home() {
   const buttons = [
-    { label: "Dashboard", href: "/dashboard" },
-    { label: "Check In/Out", href: "/check" },
-    { label: "User List", href: "/users" },
-    { label: "Setting", href: "/settings" },
+    { label: "Dashboard", icon: HiBuildingLibrary, href: "/dashboard" },
+    { label: "Check In/Out",icon: HiMiniClock, href: "/check" },
+    { label: "User List",icon: HiUser, href: "/users" },
+    { label: "Setting", icon:HiCog6Tooth, href: "/settings" },
   ];
 
   return (
@@ -13,12 +15,14 @@ export default function Home() {
       <h1 className="text-5xl font-semibold mb-12 text-black">Attendance</h1>
       <div className="grid grid-cols-2 gap-8">
         {buttons.map((btn) => (
+          
           <a
             key={btn.label}
             href={btn.href}
             className="w-40 h-20 bg-black text-white flex items-center justify-center rounded-lg text-lg font-medium shadow-md hover:bg-gray-800 transition"
           >
-            {btn.label}
+          {btn.icon && <btn.icon className="mr-2 text-2xl" />}
+          {btn.label}
           </a>
         ))}
       </div>
